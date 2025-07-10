31efb37c3fcb60280ce787126ca236f449facac4
bilibilinan <jyguojianan@gmail.com>
2025-07-10 15:57:04 +0800

    Add more pictures and improve homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./app/templates/**/*.{html,js}"],
+  content: [
+    './easyreadweb/myapp/templates/**/*.html', // 匹配所有子目录
+    './easyreadweb/myapp/static/**/*.js',
+    './templates/**/*.html', // 如果有全局模板目录
+    './app/templates/**/*.html', // 如果有其它 app
+    // 其它你实际用到的模板路径
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
